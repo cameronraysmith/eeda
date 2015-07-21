819c5db3ad77365951cecabd172bdef2e3130a6e
Cameron Smith <camrn86@gmail.com>
2015-07-21 15:44:36 -0400

    add comment

--- tds/batchgrowthdyn.m
+++ tds/batchgrowthdyn.m
@@ -15,7 +15,7 @@
 % 35    45      arbitrary test
 %-------------------------------
 
-% Test simulations
+% Test simulations (Temperature range)
 %-------------------------------
 %growthdyn(1,  2,  3,  4,  5, 6, 7, 8,   9,   10, 11, 12)
 growthdyn(33, 45, 24, 12, 10, 3, 0, 2, 0.1, 0.01, 1, 1);
